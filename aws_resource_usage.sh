eb4279dae0e0e1f89a6c1836577efca4d53d09c4
kishanmv1 <venkatkishan117@gmail.com>
2024-10-28 12:35:11 +0000

    shellprac

---
+++ aws_resource_usage.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+##############################################
+#Author: kishan
+#
+#date : 28/10/2024
+#
+#purpose : for aws resource usage
+#
+##########################################
+#AWS Ec2 Instances
+#
+#AWS S3 Buckets
+#
+#AWS Lambda Functions
+#
+#AWS IAM Users
+#
+#
+set -x
+set -o
+set -e
+
+#LISTING AWS INSTANCES
+echo  "print ec2 instance ids"
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+#Listings s3 buckets
+echo "print s3 buckets"
+aws s3 ls
+
+#delete ec2 instance
+echo "delete ec2 instance"
+aws ec2 terminate-instances --instance-id i-0598bdcfed5c204f1
+
+
